c2ebae6b436083b3917871c73878d5975341dcfb
inkspot <Wyattsimpson777@gmail.com>
2018-06-20 15:02:04 -0400

    more basic layout, js psuedocode added

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// possible words listed (more than 5, no cap)
+var wordList = [];
+
+// random word chosen
+    // tracks input from keys
+    // shows correct guesses in the word
+    // shows incorrect answers AND takes a chance away (6 chances?)
+
+    // if they answer correctly, gain a point
+
+// scoreboard showing points and remaining guesses
