1a999a9cc5ff68158113d97260417865220e63c4
inkspot <Wyattsimpson777@gmail.com>
2018-06-20 17:58:31 -0400

    rocky mtn pizza

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,43 @@
 // possible words listed (more than 5, no cap)
-var wordList = [];
+var wordList = ["gibson", "epiphone", "ibanez", "squier", "fender"];
 
-// random word chosen
+
+
+// function {
+
+function startGame() {
+
+    var wordList = wordList[Math.floor(Math.random() * wordList.length)];
+
+};
+
+// document.onkeyup = function (event) {
+
+//     var userInput = event.key;
+
+//     if (userInput === )
+
+
+// }
+
+
+// this is for reference!
+// var html =
+//     "<p>Guess: " + wordList[0] + "</p>" +
+//     "<p>Computer chose: " + wordList[1] + "</p>" +
+//     "<p>Wins: " + wordList[2 ] + "</p>" +
+//     "<p>Losses: " + wordList[3] + "</p>" +
+//     "<p>Ties: " + wordList[4] + "</p>";
+
+// document.querySelector("#game").innerHTML = html;
+
+    // random word chosen
     // tracks input from keys
     // shows correct guesses in the word
     // shows incorrect answers AND takes a chance away (6 chances?)
 
     // if they answer correctly, gain a point
+
+// }
 
 // scoreboard showing points and remaining guesses
