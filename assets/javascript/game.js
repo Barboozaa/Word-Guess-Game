0191be8657e9f99d3d4b0d1b6e28670a77094ca7
inkspot <Wyattsimpson777@gmail.com>
2018-06-21 17:46:19 -0400

    rockky mtn pizza pt2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,61 @@
 // possible words listed (more than 5, no cap)
 var wordList = ["gibson", "epiphone", "ibanez", "squier", "fender"];
 
+var getRandomWord = function () {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+};
+
+// document.getElementById("startButton").addEventListener("click", startGame);
 
 
-// function {
+// listen to start button, when it's hit start the game
+
+document.getElementById("startButton").addEventListener("click", startGame());
 
 function startGame() {
+    // document.getElementById("startButton");
 
-    var wordList = wordList[Math.floor(Math.random() * wordList.length)];
+    var word = getRandomWord();
+    console.log(word);
 
-};
+    var h3 = document.createElement("h3");
+    var div = document.getElementsById("game");
+    document.appendChild(h3);
+
+    // document.wordDisplayed.appendChild(div);
+
+
+    //     var p = document.createElement("p");
+    // document.body.appendChild(p);
+    // console.log(word)
+}
+
+
+            // Creates a Function called "vowelChecker" that takes in a single argument (x).
+            // function vowelChecker(x) {
+
+            //     // vowelChecker will grab the first letter (character)...
+            //     var firstChar = x.toLowerCase().charAt(0);
+
+            //     // Then check if that first letter is a vowel.
+            //     if (firstChar === "a" || firstChar === "e" || firstChar === "i" || firstChar === "o" || firstChar === "u") {
+
+            //         // If so... it will log that.
+            //         console.log("OMG. The first letter is a vowel!");
+
+            //     }
+
+            //     else {
+
+            //         // If not... it will log that.
+            //         console.log("First letter is NOT a vowel. *sadface*");
+
+            //     }
+            // }
+
+
+
+
 
 // document.onkeyup = function (event) {
 
@@ -21,15 +67,7 @@ function startGame() {
 // }
 
 
-// this is for reference!
-// var html =
-//     "<p>Guess: " + wordList[0] + "</p>" +
-//     "<p>Computer chose: " + wordList[1] + "</p>" +
-//     "<p>Wins: " + wordList[2 ] + "</p>" +
-//     "<p>Losses: " + wordList[3] + "</p>" +
-//     "<p>Ties: " + wordList[4] + "</p>";
 
-// document.querySelector("#game").innerHTML = html;
 
     // random word chosen
     // tracks input from keys
