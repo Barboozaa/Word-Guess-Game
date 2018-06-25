cc529950bb997e759b37544404d30b64ba3ce64f
inkspot <Wyattsimpson777@gmail.com>
2018-06-25 18:12:20 -0400

    ahh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,77 @@
 // possible words listed (more than 5, no cap)
 var wordList = ["dig dug", "galaga", "pac man", "space invaders", "donkey kong", "tetris"];
 
+var word = "";
+
+var lettersInWord = [];
+
+var numBlanks = 0;
+
+var incorrectGuesses = [];
+
+var wins = 0;
+var losses = 0;
+var guesses = 6;
+
+
 function getRandomWord() {
     return wordList[Math.floor(Math.random() * wordList.length)];
 };
+
+// EVERYTHING IS FUCKED! Refactor everything under here to work with the new gobal vars
+// Look at the word guess game in the New activities folder in the GTA repo
 
 function startGame() {
 
     document.getElementById("startButton").style.visibility = "hidden";
     
-    var word = getRandomWord();
-
     var wordBlank = document.getElementById("wordBox");
 
-    var node = document.createTextNode(word);
+    var node = document.createTextNode(getRandomWord());
     
     wordBlank.appendChild(node);
+
+    game();
+
+    function game() {
+
+        var correctLetter = 0;
     
-};
-
-function game() {
-
-    var correctLetter = 0;
-
-    document.onkeyup = function(event) {
-
-        var userGuess = event.key;
-
-        for (var i = 0; i < word.length; i++) {
-
-            if (word[i] === userGuess) {
+        document.onkeyup = function(event) {
     
-                word[i].visibility = true; // this is more psuedocode than real right now
+            var userGuess = event.key;
     
-                correctLetter++;
+            for (var i = 0; i < word.length; i++) {
     
-            }
-    
+                if (word[i] === userGuess) {
+        
+                    word[i].visibility = true; // this is more psuedocode than real right now
+        
+                    correctLetter++;
+        
+                }
+        
+            };
+        
+            if (correctLetter == 0) { // it's logging the incorrect letter until a correct letter is input
+                                        // I'm p sure correctLetter is getting incremented and not resetting (thanks Hunter)
+                var incorrectGuessList = document.getElementById("incorrectGuesses");
+
+                var incorrectGuess = document.createTextNode(userGuess + ", ");
+
+                incorrectGuessList.appendChild(incorrectGuess);
+        
+            };
+        
         };
     
-        if (correctLetter == 0) {
-    
-            // add input to incorrect guess list
-    
-        };
+        
     
     };
-
     
-
 };
+
+
 
 
 
